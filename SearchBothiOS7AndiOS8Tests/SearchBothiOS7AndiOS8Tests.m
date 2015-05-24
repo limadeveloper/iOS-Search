286c1652a5c40bdf5eeb7e0f398200c5cf903ec7
John Silva <Johncarlosboy@gmail.com>
2015-05-24 18:15:01 -0300

    initial commit

---
+++ SearchBothiOS7AndiOS8Tests/SearchBothiOS7AndiOS8Tests.m
@@ -0,0 +1,40 @@
+//
+//  SearchBothiOS7AndiOS8Tests.m
+//  SearchBothiOS7AndiOS8Tests
+//
+//  Created by John Silva on 5/24/15.
+//  Copyright (c) 2015 John Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SearchBothiOS7AndiOS8Tests : XCTestCase
+
+@end
+
+@implementation SearchBothiOS7AndiOS8Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
