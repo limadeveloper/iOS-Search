fa86e3142710836531eb0b31b649e7ef5b2b0eab
John Lima <john.limadeveloper@gmail.com>
2017-04-18 23:38:11 -0300

    # Model;

---
+++ Objective-C/Search/Model/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  Search
+//
+//  Created by John Lima on 18/04/17.
+//  Copyright © 2017 John Silva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ Objective-C/Search/Model/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  Search
+//
+//  Created by John Lima on 18/04/17.
+//  Copyright © 2017 John Silva. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
